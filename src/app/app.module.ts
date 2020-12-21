b8ce0aa03ff968c9e61af342b7068c326340b45c
Maida64 <maida.ijaz@educative.io>
2020-12-21 19:38:01 +0500

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClientModule } from './clients/client.module';
+import { CompanyModule } from './company/company.module';
+import { SharedModule } from './shared/shared.module';
+import { CustomMaterialModule } from './custom-material.module';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AboutModule } from './about/about.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ClientService } from './clients/client.service';
+
+@NgModule({
+  declarations: [
+    AppComponent, NavigationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ClientModule,
+    CompanyModule,
+    SharedModule,
+    CustomMaterialModule,
+    AboutModule, 
+    HttpClientModule,
+  ],
+  providers: [ClientService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
