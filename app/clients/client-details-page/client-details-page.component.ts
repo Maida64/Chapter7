3d07699964053cb895b671bcc81866d23eb2176f
Maida64 <maida.ijaz@educative.io>
2020-12-21 21:18:12 +0500

    first cit

--- app/clients/client-details-page/client-details-page.component.ts
+++ app/clients/client-details-page/client-details-page.component.ts
@@ -48,3 +48,4 @@ export class ClientDetailsPageComponent implements OnInit {
     this.router.navigateByUrl('/clients/search');
   }
 }
+
--- app/clients/client-search-page/client-search-page.component.ts
+++ app/clients/client-search-page/client-search-page.component.ts
@@ -36,3 +36,4 @@ export class ClientSearchPageComponent implements OnInit {
     });
   }
 }
+
